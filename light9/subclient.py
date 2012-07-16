8f48827114ff525ee89719fd3d0c678a47b98e61
drewp <drewp@bigasterisk.com>
2012-07-16 15:05:35 -0700

    reformat
    Ignore-this: 7693a606db3bf0d9fbc0ca561a875d2e
    
    darcs-hash:20120716220535-312f9-f56289db7fb3aaf10ede27fcafee6b8bb28d01a1.gz

--- light9/subclient.py
+++ light9/subclient.py
@@ -5,22 +5,28 @@ from light9.Submaster import Submaster
 class SubClient:
     def __init__(self):
         pass # we may later need init code for network setup
+
     def get_levels_as_sub(self):
         """Subclasses must implement this method and return a Submaster
         object."""
+
     def get_dmx_list(self):
         maxes = self.get_levels_as_sub()
         return maxes.get_dmx_list()
+
     def send_sub(self, sub):
         levels = sub.get_dmx_list()
         dmxclient.outputlevels(levels)
+
     def send_levels(self):
         levels = self.get_dmx_list()
         dmxclient.outputlevels(levels)
+
     def send_levels_loop(self, delay=1000):
         """This function assumes that we are an instance of a Tk object
         (or at least that we have an 'after' method)"""
         self.send_levels()
         self.after(delay, self.send_levels_loop, delay)
+
     def send_zeroes(self):
         self.send_sub(Submaster('empty', {}, temporary=1))
