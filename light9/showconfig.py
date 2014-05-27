779fc2393c8292f9cc12daf004b8a2058854ddb0
Drew Perttula <drewp@bigasterisk.com>
2014-05-27 00:31:44 -0700

    dead code
    Ignore-this: 74dd742c8e670f1c26ad0cd55fc62b3b
    
    darcs-hash:20140527073144-836b1-37e3a550a656bf48f55b29ec642620d026516b29

--- light9/showconfig.py
+++ light9/showconfig.py
@@ -1,4 +1,4 @@
-import time, logging, warnings
+import logging, warnings
 from twisted.python.filepath import FilePath
 from os import path, getenv
 from rdflib import Graph
@@ -13,8 +13,11 @@ def getGraph():
     global _config
     if _config is None:
         graph = Graph()
+        # note that logging is probably not configured the first time
+        # we're in here
+        print "reading n3 files around %r" % root()
         for f in FilePath(root()).globChildren("*.n3") + FilePath(root()).globChildren("build/*.n3"):
-            log.info("reading %s", f)
+            print "reading %s" % f
             graph.parse(location=f.path, format='n3')
         _config = graph
     return _config
@@ -30,31 +33,13 @@ def showUri():
     """Return the show URI associated with $LIGHT9_SHOW."""
     return URIRef(file(path.join(root(), 'URI')).read().strip())
 
-def findMpdHome():
-    """top of the music directory for the mpd on this system,
-    including trailing slash"""
-    
-    mpdHome = None
-    for mpdConfFilename in ["/my/dl/modified/mpd/src/mpdconf-testing",
-                            "~/.mpdconf", "/etc/mpd.conf"]:
-        try:
-            mpdConfFile = open(path.expanduser(mpdConfFilename))
-        except IOError:
-            continue
-        for line in mpdConfFile:
-            if line.startswith("music_directory"):
-                mpdHome = line.split()[1].strip('"')
-                return mpdHome.rstrip(path.sep) + path.sep  
-
-    raise ValueError("can't find music_directory in any mpd config file")
-
 def songOnDisk(song):
     """given a song URI, where's the on-disk file that mpd would read?"""
     graph = getGraph()
     root = graph.value(showUri(), L9['musicRoot'])
     if not root:
         raise ValueError("%s has no :musicRoot" % showUri())
-    
+
     name = graph.value(song, L9['songFilename'])
     if not name:
         raise ValueError("Song %r has no :songFilename" % song)
@@ -104,19 +89,8 @@ def getSongsFromShow(graph, show):
 def curvesDir():
     return path.join(root(),"curves")
 
-def songFilename(song):
-    return path.join(root(), "music", "%s.wav" % song)
-
-def subtermsForSong(song):
-    return path.join(root(),"subterms",song)
-
 def subFile(subname):
     return path.join(root(),"subs",subname)
 
 def subsDir():
     return path.join(root(),'subs')
-
-def prePostSong():
-    graph = getGraph()
-    return [graph.value(MUS['preSong'], L9['showPath']),
-            graph.value(MUS['postSong'], L9['showPath'])]
