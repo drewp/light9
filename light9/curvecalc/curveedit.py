dc6f062cbc09c647573c97b4e73922c29fd03468
Drew Perttula <drewp@bigasterisk.com>
2014-06-01 17:21:33 -0700

    switch curvecalc web server to cyclone
    Ignore-this: 6d72b5c9fdcea4473f96176df63b95ec
    
    darcs-hash:20140602002133-836b1-f97ee08eb6e957b6cbaacf213d79ef47aede2096

--- light9/curvecalc/curveedit.py
+++ light9/curvecalc/curveedit.py
@@ -2,19 +2,20 @@
 this may be split out from curvecalc someday, since it doesn't
 need to be tied to a gui """
 from twisted.internet import reactor
-from light9 import networking
+import cyclone.web, cyclone.httpclient, cyclone.websocket
+
+from lib.cycloneerr import PrettyErrorHandler
+
 
 def serveCurveEdit(port, hoverTimeResponse):
     """
     /hoverTime requests actually are handled by the curvecalc gui
     """
-                       
-    from twisted.web import server, resource
-    class Hover(resource.Resource):
-        isLeaf = True
-        def render_GET(self, request):
-            if request.path == '/hoverTime':
-                return hoverTimeResponse(request)
-            raise NotImplementedError()
 
-    reactor.listenTCP(port, server.Site(Hover()))
+    class HoverTime(PrettyErrorHandler, cyclone.web.RequestHandler):
+        def get(self):
+            hoverTimeResponse(self)
+
+    reactor.listenTCP(port, cyclone.web.Application(handlers=[
+        (r'/hoverTime', HoverTime),
+        ], debug=True))
