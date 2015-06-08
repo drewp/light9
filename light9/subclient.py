37a085a93c27e4ff26e73bbe30d7306971766f45
Drew Perttula <drewp@bigasterisk.com>
2015-06-08 00:13:44 -0700

    remove old code 'gyrocontroller'
    Ignore-this: c6ef242d545b18d19a185917c52966ea
    
    darcs-hash:20150608071344-836b1-5494d6047e8179e5a3258e21e544fd68feea1be9

--- light9/subclient.py
+++ light9/subclient.py
@@ -1,7 +1,5 @@
 from light9 import dmxclient
-from light9.Submaster import Submaster
 
-# later, this stuff will talk to a SubServer
 class SubClient:
     def __init__(self):
         """assumed that your init saves self.graph"""
@@ -11,20 +9,8 @@ class SubClient:
         """Subclasses must implement this method and return a Submaster
         object."""
 
-    def get_dmx_list(self):
-        maxes = self.get_levels_as_sub()
-        return maxes.get_dmx_list()
-
-    def send_sub(self, sub):
-        levels = sub.get_dmx_list()
-        dmxclient.outputlevels(levels)
-
     def send_levels(self):
-        self.graph.addHandler(self._send_levels_handler)
-        
-    def _send_levels_handler(self):
-        levels = self.get_dmx_list()
-        dmxclient.outputlevels(levels)
+        self.graph.addHandler(self._send_sub)
 
     def send_levels_loop(self, delay=1000):
         """This function assumes that we are an instance of a Tk object
@@ -32,5 +18,7 @@ class SubClient:
         self.graph.addHandler(self.send_levels)
         self.after(delay, self.send_levels_loop, delay)
 
-    def send_zeroes(self):
-        self.send_sub(Submaster('empty', {}, temporary=1))
+    def _send_sub(self):
+        maxes = self.get_levels_as_sub()
+        levels = maxes.get_dmx_list()
+        dmxclient.outputlevels(levels)
