a94d83e0c194c3022a78ba337377bb80162752b0
drewp <drewp@bigasterisk.com>
2018-06-10 21:56:14 -0700

    raise qpan ry a little bit
    Ignore-this: a024cabca35c9b753116fa0b304f63c3
    (but probably needs a bit more...maybe 0.6?)
    
    darcs-hash:545a2efff59ec666b1c1b79be2f5d134ec1d228e

--- light9/effect/effecteval.py
+++ light9/effect/effecteval.py
@@ -138,7 +138,7 @@ def effect_qpan(effectSettings, strength, songTime, noteTime):
         (dev, L9['color']): col,
         (dev, L9['focus']): 0.589,
         (dev, L9['rx']): lerp(0.778, 0.291, clamp(0, 1, noteTime / dur)),
-        (dev, L9['ry']): 0.383,
+        (dev, L9['ry']): 0.5,
         (dev, L9['zoom']): 0.714,
     }
 
